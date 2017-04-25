936a16afaa3380e210c85a1ad0b3986af3310f0a
Alberto Silva <albertossilva@gmail.com>
2017-04-25 14:52:23 -0300

    Resolving promise by methods

--- src/defaultHandlers/promise.js
+++ src/defaultHandlers/promise.js
@@ -1,14 +1,10 @@
 import consts from '../consts';
 import Handler from '../Handler';
 
-
-const checker = function(value){
-  if (Promise){
-    return value instanceof Promise;
-  }
-
-  return value.hasOwnProperty('then') && value.hasOwnProperty('catch');
-};
+const checker = value =>
+  value &&
+  value.hasOwnProperty('then') &&
+  value.hasOwnProperty('catch');
 
 const resolver = function(value, res, delayedSolver){
   value.then(v=>delayedSolver(v, res, delayedSolver));
